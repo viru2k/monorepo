95c4252f6e5c32c2dc35f5b9d51e45ddcbd72ad5
viru2k <gastonvillafane@gmail.com>
2025-03-19 19:29:26 +0100

    add repo

--- backoffice-monorepo/apps/backoffice-monorepo/src/app/app.component.ts
+++ backoffice-monorepo/apps/backoffice-monorepo/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StorybookComponentsComponent } from './private/storybook/storybook.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [RouterModule, BrowserModule,BrowserAnimationsModule, CommonModule, StorybookComponentsComponent
+  imports: [RouterModule, CommonModule, StorybookComponentsComponent
   ],
   selector: 'app-root',
   templateUrl: './app.component.html',
